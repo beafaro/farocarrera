55d321bf5e757daefd385ac974ef167a7730d4ed
Beatriz Faro Carrera <farocarrera.beatriz@gmail.com>
2022-01-18 12:58:43 +0100

    tabla ventas y facturas

--- main.py
+++ main.py
@@ -150,7 +150,6 @@ class Main(QtWidgets.QMainWindow):
         var.ui.cmbProv.currentIndexChanged.connect(conexion.Conexion.selMuni)
         conexion.Conexion.cargarCmbProducto(self)
         var.cmbProducto.currentIndexChanged.connect(invoice.Facturas.procesoVenta)
-        #f
 
 
         '''
